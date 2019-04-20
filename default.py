d50f9939c72f1e101d809365caba59d541d24fe5
SolAZDev <SolAZDev@gmail.com>
2019-04-20 07:00:04 -0400

    It works (on kodi, but not on xbox)

--- default.py
+++ default.py
@@ -1,6 +1,6 @@
 #! /usr/bin/python2.7
 # XBMC-Kai by SolAZDev
-#import xbmc
+# import xbmc
 import requests
 import xbmc
 import xbmcgui
@@ -9,29 +9,14 @@ aPrevMenu = 10
 aSelectItem = 7
 
 
-class XBMCKai(xbmcgui.Window):
-    def onAction(self, action):
-        if action == aPrevMenu:
-            self.close()
-        if action == aSelectItem:
-            res = KaiConnect.GetFromKai('getStatus')
-            self.strAction = xbmcgui.ControlLabel(
-                300, 200, 200, 500, res, 'font14', '0xFF00FF00')
-            self.addControl(self.strAction)
-
-
 class KaiConnect:
     """Connection to Kai Engine via IP and Port."""
     kaiIP = '10.0.52.103'
-    kaiPort = '34522'
+    kaiPort = 34522
     kaiPortFoward = 30000  # Doubt I'll need this
 
-    kaiIP.__doc__ = "Kai Engine's IP Address"
-    kaiPort.__doc__ = "Kai Engine's WebUI Port. Max of 5 digits"
-    kaiPortFoward.__doc__ = "Unused So far. Kai Engine's Portfoward port."
-
     def CheckForKai(self):
-        url = 'http://'+self.kaiIP+':'+self.kaiPort+'/api/v1/isalive'
+        url = 'http://'+self.kaiIP+':'+`self.kaiPort`+'/api/v1/isalive'
         print('Trying to get '+url)
         r = requests.get(url)
         r.status_code
@@ -41,12 +26,12 @@ class KaiConnect:
             print(r.text)
 
     def GetFromKai(self, getCall):
-        url = 'http://'+self.kaiIP+':'+self.kaiPort+'/api/v1/'+getCall
+        url = 'http://'+self.kaiIP+':'+`self.kaiPort`+'/api/v1/'+getCall
         print('Trying to get '+url)
         r = requests.get(url)
         print(r.status_code)
         if r.status_code != 200:
-            print("Wrong address, try again, Error Code: "+r.status_code)
+            print("Wrong address, try again, Error Code: "+`r.status_code`)
         else:
             return r.text
             # print(r.text)
@@ -57,14 +42,40 @@ class KaiConnect:
         print(self.GetFromKai('getvector'))
 
 
+class MainMenu(xbmcgui.Window):
+    def __init__(self):
+        kai = KaiConnect()
+        res = kai.GetFromKai('getstatus')
+        self.strHead = xbmcgui.ControlLabel(
+            250, 80, 200, 50, '', 'font14', '0xFF00FF00')
+        self.addControl(self.strHead)
+        self.strHead.setLabel('XMBC-Kai Plugin')
+
+        self.strStatus = xbmcgui.ControlLabel(
+            250, 250, 400, 400, '', 'font14', '0xFF00FF00')
+        self.addControl(self.strStatus)
+        self.strStatus.setLabel(res)
+
+    def onAction(self, action):
+        kai = KaiConnect()
+        if action == aPrevMenu:
+            self.close()
+        if action == aSelectItem:
+            d = xbmcgui.Dialog()
+            d.ok('It works?', 'Kai\'s link is... ' +
+                 kai.GetFromKai('getstatus'))
+
+
 # if __name__ == '__main__':
 #     kai = KaiConnect()
 #     kai.main()
 
-diag = xbmcgui.Dialog()
-diag.ok(KaiConnect.GetFromKai('getstatus'))
+# diag = xbmcgui.Dialog()
+# kai = KaiConnect()
+# res = kai.GetFromKai('getstatus')
+# diag.ok('result', res)
 
 
-# xkWindow = XBMCKai()
-# xkWindow.doModal()
-# del xkWindow
+xkWindow = MainMenu()
+xkWindow.doModal()
+del xkWindow
